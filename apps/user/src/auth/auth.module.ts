3061b6d110875c478156995a7c0c76cbcf85db36
JunPyo0117 <vksehfkdydy@naver.com>
2026-02-03 17:29:58 +0900

    Implement authentication logic in user module, adding login functionality and enhancing registration with validation. Integrate JWT for token management and update configuration validation for security tokens.

--- apps/user/src/auth/auth.module.ts
+++ apps/user/src/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { User } from '../user/entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    JwtModule.register({}),
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/user/src/auth/auth.service.ts
+++ apps/user/src/auth/auth.service.ts
@@ -1,10 +1,22 @@
-import { BadGatewayException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { RegisterDto } from './dto/register-dto';
+import { Repository } from 'typeorm';
+import { User } from '../user/entity/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(rawToken: string, registerDto: RegisterDto) {
     const { email, password } = this.parseBasicToken(rawToken);
@@ -21,13 +33,13 @@ export class AuthService {
     const basicSplit = rawToken.split(' ');
 
     if (basicSplit.length !== 2) {
-      throw new BadGatewayException('토큰 포맷이 잘못됐습니다.');
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다.');
     }
 
     const [basic, token] = basicSplit;
 
     if (basic.toLowerCase() !== 'basic') {
-      throw new BadGatewayException('토큰 포맷이 잘못됐습니다.');
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다.');
     }
 
     const decoded = Buffer.from(token, 'base64').toString('utf-8');
@@ -35,11 +47,66 @@ export class AuthService {
     const tokenSplit = decoded.split(':');
 
     if (tokenSplit.length !== 2) {
-      throw new BadGatewayException('토큰 포맷이 잘못됐습니다.');
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다.');
     }
 
     const [email, password] = tokenSplit;
 
     return { email, password };
+  }
+
+  async login(rawToken: string) {
+    const { email, password } = this.parseBasicToken(rawToken);
+
+    const user = await this.authenticate(email, password);
+
+    return {
+      refreshToken: await this.issueToken(user, true),
+      accessToken: await this.issueToken(user, false),
+    };
+  }
+
+  async authenticate(email: string, password: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: {
+        id: true,
+        email: true,
+        password: true,
+      },
+    });
+
+    if (!user) {
+      throw new BadRequestException('잘못된 로그인 정보입니다.');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new BadRequestException('잘못된 로그인 정보입니다.');
+    }
+
+    return user;
+  }
+
+  async issueToken(user: any, isRefresh: boolean) {
+    const refreshTokenSecret = this.configService.getOrThrow<string>(
+      'REFRESH_TOKEN_SECRET',
+    );
+    const accessTokenSecret = this.configService.getOrThrow<string>(
+      'ACCESS_TOKEN_SECRET',
+    );
+
+    return this.jwtService.signAsync(
+      {
+        sub: user.id ?? user.sub,
+        role: user.role,
+        type: isRefresh ? 'refresh' : 'access',
+      },
+      {
+        secret: isRefresh ? refreshTokenSecret : accessTokenSecret,
+        expiresIn: '3600h',
+      },
+    );
   }
 }
